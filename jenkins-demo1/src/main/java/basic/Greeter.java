09f5757773730824d19cc305ae70d0bf8c609404
Tianyi.Huang <fh.frankhuang@gmail.com>
2024-03-21 22:44:52 +0000

    add a unit test

---
+++ jenkins-demo1/src/main/java/basic/Greeter.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class Greeter {
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
+
---
+++ jenkins-demo1/src/test/java/basic/GreeterTest.java
@@ -0,0 +1,14 @@
+package basic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GreeterTest {
+
+    @Test
+    public void testGreet() {
+        Greeter greeter = new Greeter();
+        assertEquals("Hello, World!", greeter.greet("World"));
+    }
+}
