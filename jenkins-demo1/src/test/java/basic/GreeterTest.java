2f9bca3a0a99146bb1160793162e180dacacde30
Tianyi.Huang <fh.frankhuang@gmail.com>
2024-03-22 01:10:25 +0000

    remove public modifier of class in Junit

--- jenkins-demo1/src/test/java/basic/GreeterTest.java
+++ jenkins-demo1/src/test/java/basic/GreeterTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class GreeterTest {
+class GreeterTest {
 
     @Test
     void testGreet() {
